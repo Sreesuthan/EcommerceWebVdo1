9941a237f243a1a03a13ee55baafdd569c67ffd0
Sreesuthan <sreesuthan02@gmail.com>
2022-11-17 21:49:11 +0530

    Localstorage,Shopping cart

--- EcommerceWebVdo1/Client/Program.cs
+++ EcommerceWebVdo1/Client/Program.cs
@@ -1,4 +1,7 @@
+using Blazored.LocalStorage;
+using Blazored.Toast;
 using EcommerceWebVdo1.Client;
+using EcommerceWebVdo1.Client.Services.CartService;
 using EcommerceWebVdo1.Client.Services.CategoryServices;
 using EcommerceWebVdo1.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
@@ -11,4 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
 await builder.Build().RunAsync();
